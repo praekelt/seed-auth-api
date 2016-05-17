0eeaf8e99040cdf21b96a4111ab4449d056ce4bc
Rudi Giesler <rudi@praekelt.com>
2016-05-17 14:16:17 +0200

    Add test to make sure that tests are running.

--- authapi/tests.py
+++ authapi/tests.py
@@ -1,1 +1,7 @@
-# Create your tests here.
+from django.test import TestCase
+
+
+class RunningTests(TestCase):
+    def test_tests_are_run(self):
+        '''Make sure that our tests are actually running.'''
+        pass
