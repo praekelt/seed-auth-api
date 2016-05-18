d0b445f75ed3277c932b7ac51a31e35526e175d9
Rudi Giesler <rudi@praekelt.com>
2016-05-18 17:06:22 +0200

    Add skeleton models and migrations for those models

--- authapi/models.py
+++ authapi/models.py
@@ -1,1 +1,19 @@
-# Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class SeedOrganization(models.Model):
+    # TODO: Implement SeedOrganization model
+    users = models.ManyToManyField(User)
+
+
+class SeedPermission(models.Model):
+    # TODO: Implement SeedPermission model
+    pass
+
+
+class SeedTeam(models.Model):
+    # TODO: Implement SeedTeam model
+    organization = models.ForeignKey(SeedOrganization)
+    users = models.ManyToManyField(User)
+    permissions = models.ManyToManyField(SeedPermission)
