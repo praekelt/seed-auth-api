37a64df3e5fdb8289a7e30a1817ae487bdc59e5b
Rudi Giesler <rudi@praekelt.com>
2016-05-20 17:28:40 +0200

    Add view for adding and removign permissions from a team

--- authapi/views.py
+++ authapi/views.py
@@ -3,10 +3,10 @@ from rest_framework import viewsets, status
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 
-from authapi.models import SeedOrganization, SeedTeam
+from authapi.models import SeedOrganization, SeedTeam, SeedPermission
 from authapi.serializers import (
     OrganizationSerializer, TeamSerializer, UserSerializer,
-    OrganizationUserSerializer)
+    OrganizationUserSerializer, PermissionSerializer)
 
 
 class OrganizationViewSet(viewsets.ModelViewSet):
@@ -77,6 +77,27 @@ class TeamViewSet(viewsets.ModelViewSet):
         team = self.get_object()
         team.archived = True
         team.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class TeamPermissionViewSet(viewsets.ViewSet):
+    '''Nested viewset to add and remove permissions from teams.'''
+    def create(self, request, team_pk=None):
+        '''Add a permission to a team.'''
+        serializer = PermissionSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        team = get_object_or_404(SeedTeam, pk=team_pk)
+        permission = team.permissions.create(**serializer.data)
+        serializer = PermissionSerializer(permission)
+        return Response(serializer.data)
+
+    def destroy(self, request, pk=None, team_pk=None):
+        '''Remove a permission from a team.'''
+        team = get_object_or_404(SeedTeam, pk=team_pk)
+        permission = get_object_or_404(SeedPermission, pk=pk)
+        team.permissions.remove(permission)
+        permission.archived = True
+        permission.save()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
