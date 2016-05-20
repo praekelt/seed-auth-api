b4505d6b3dd9ffc0f8892a2b6a00990743524d28
Rudi Giesler <rudi@praekelt.com>
2016-05-20 17:59:09 +0200

    Add urls for user add/remove from team

--- authapi/urls.py
+++ authapi/urls.py
@@ -20,8 +20,15 @@ permissions_router.register(
     r'permissions', views.TeamPermissionViewSet,
     base_name='seedteam-permissions')
 
+teamusers_router = routers.NestedSimpleRouter(
+    router, r'teams', lookup='team')
+teamusers_router.register(
+    r'users', views.TeamUsersViewSet,
+    base_name='seedteam-users')
+
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^', include(orgusers_router.urls)),
     url(r'^', include(permissions_router.urls)),
+    url(r'^', include(teamusers_router.urls)),
 ]
