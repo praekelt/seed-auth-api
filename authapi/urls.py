225a558a2dd11bc1a5d900c613fed1895637426a
Rudi Giesler <rudi@praekelt.com>
2016-05-20 15:51:51 +0200

    Add org user add/remove view to url

--- authapi/urls.py
+++ authapi/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import include, url
-from rest_framework import routers
+from rest_framework_nested import routers
 
 from authapi import views
 
@@ -8,6 +8,13 @@ router.register(r'organizations', views.OrganizationViewSet)
 router.register(r'teams', views.TeamViewSet)
 router.register(r'users', views.UserViewSet)
 
+orgusers_router = routers.NestedSimpleRouter(
+    router, r'organizations', lookup='organization')
+orgusers_router.register(
+    r'users', views.OrganizationUsersViewSet,
+    base_name='seedorganization-users')
+
 urlpatterns = [
     url(r'^', include(router.urls)),
+    url(r'^', include(orgusers_router.urls)),
 ]
