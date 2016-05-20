3ebdc894abdfc0d27268124877ebf604d66a9d1c
Rudi Giesler <rudi@praekelt.com>
2016-05-20 17:29:03 +0200

    Add urls for adding and removing permissions from teams

--- authapi/urls.py
+++ authapi/urls.py
@@ -14,7 +14,14 @@ orgusers_router.register(
     r'users', views.OrganizationUsersViewSet,
     base_name='seedorganization-users')
 
+permissions_router = routers.NestedSimpleRouter(
+    router, r'teams', lookup='team')
+permissions_router.register(
+    r'permissions', views.TeamPermissionViewSet,
+    base_name='seedteam-permissions')
+
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^', include(orgusers_router.urls)),
+    url(r'^', include(permissions_router.urls)),
 ]
