b126fabf4e84f1f8ce057be2e9440c0e1cfd8ea6
requires.io <support@requires.io>
2016-11-01 14:12:49 +0100

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
         'Django==1.10.2',
         'dj_database_url==0.4.1',
         'psycopg2cffi==2.7.5',
-        'djangorestframework==3.5.1',
+        'djangorestframework==3.5.2',
         'drf-extensions==0.3.1',
         'djangorestframework-composed-permissions==0.1',
         'raven==5.31.0',
