a31e48e8f841e51839e15f1611e4a38e2fd903ff
requires.io <support@requires.io>
2018-05-02 07:50:50 +0100

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     packages=find_packages(),
     include_all_package_data=True,
     install_requires=[
-        'Django==2.0.4',
+        'Django==2.0.5',
         'dj-database-url==0.5.0',
         'psycopg2==2.7.4',
         'djangorestframework==3.8.2',
